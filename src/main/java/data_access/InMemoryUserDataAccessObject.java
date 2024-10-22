0463bea913b27b722487256464cad1096abb9957
Ryan Hu <ryhu@student.unimelb.edu.au>
2024-10-22 18:13:06 -0400

    Self task correction 3

--- src/main/java/data_access/InMemoryUserDataAccessObject.java
+++ src/main/java/data_access/InMemoryUserDataAccessObject.java
@@ -42,7 +42,7 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
 
     @Override
     public String getCurrentUser() {
-        return null;
+        return currentUser;
     }
 
     @Override
